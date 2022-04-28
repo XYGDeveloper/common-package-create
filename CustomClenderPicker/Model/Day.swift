5d0154a920a764d6d1821cbc844d6c718e9ef9e0
xiyg <xyg15243228311@163.com>
2022-04-29 00:14:23 +0800

    - cocopods

---
+++ CustomClenderPicker/Model/Day.swift
@@ -0,0 +1,20 @@
+//
+//  Day.swift
+//  CustomClenderPicker
+//
+//  Created by xyg on 2022/4/28.
+//
+
+import Foundation
+
+struct Day {
+  // 1
+  let date: Date
+  // 2
+  let number: String
+  // 3
+  let isSelected: Bool
+  // 4
+  let isWithinDisplayedMonth: Bool
+}
+
---
+++ CustomClenderPicker/Model/MonthMetadata.swift
@@ -0,0 +1,14 @@
+//
+//  MonthMetadata.swift
+//  CustomClenderPicker
+//
+//  Created by xyg on 2022/4/28.
+//
+
+import Foundation
+
+struct MonthMetadata {
+  let numberOfDays: Int
+  let firstDay: Date
+  let firstDayWeekday: Int
+}
